31dac3f00abb4f0dd639b09027790ef577ff3156
Documenter.jl <documenter@juliadocs.github.io>
2024-08-15 23:02:24 +0000

    build based on f7eeb63

---
+++ previews/PR26/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FiberNlse","category":"page"},{"location":"#FiberNlse","page":"Home","title":"FiberNlse","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FiberNlse.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FiberNlse]","category":"page"}]
+}
